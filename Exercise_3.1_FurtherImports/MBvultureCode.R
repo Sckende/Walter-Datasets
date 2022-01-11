a7067e498fc0084d28188637a4f77cef54337fd2
Claire-Cecile Juhasz <ccjuhasz@campus.univ.run>
2022-01-11 12:09:47 +0400

    # MoveBank API try

--- Exercise_3.1_FurtherImports/MBvultureCode.R
+++ Exercise_3.1_FurtherImports/MBvultureCode.R
@@ -1,14 +1,25 @@
 library(move)
 library(RCurl)
 library(circular)
+source('iden.r')
+login <- movebankLogin(username = ID,
+                       password = PWD)
+getMovebankStudies(login)
 
-login <- movebankLogin(username="wdwalter", password="xxxxxx")
+ind.infos <- getMovebankAnimals(study = "Turkey vultures in North and South America (data from Dodge et al. 2014)",
+                                login = login)
 
-getMovebankAnimals(study="Turkey Vulture South Carolina USA",login=login)
-
-turkey <- getMovebankData(study="Turkey Vulture South Carolina USA", animalName=c("Bird51","Bird52","Bird54","Bird55a","Bird56","Bird59a","Bird60a"),login=login, moveObject=TRUE)
+turkey <- getMovebankData(study = "Turkey vultures in North and South America (data from Dodge et al. 2014)",
+                          login = login,
+                          animalName = c("Argentina",
+                                         "Butterball",
+                                         "Morongo",
+                                         "Prado",
+                                         "Sarkis",
+                                         "Domingo",
+                                         "La Pampa"),
+                          moveObject = TRUE)
 
 n.locs(turkey)
-# Bird51  Bird52  Bird54 Bird55a  Bird56 Bird59a Bird60a 
-#   9655   11456    2378    2228    5876    8311    8594 
-   
+# Argentina Butterball    Domingo   La.Pampa    Morongo      Prado     Sarkis 
+#     4058       1275       3235       4032      19868      20967       8451 
